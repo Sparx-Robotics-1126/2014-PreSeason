6ad44911ef5802329356031be38550f6a4c49fc7
Justin Bassett <Bassett.JustinT@gmail.com>
2013-11-06 18:59:47 -0500

    Initial commit for testing project.

---
+++ DemoingProject/src/demoingproject/DemoingProject.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package demoingproject;
+
+/**
+ *
+ * @author jbass
+ */
+public class DemoingProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] unsorted1 = new int[10];
+        int[] unsorted2 = new int[10];
+        int[] unsorted3 = new int[10];
+        //int[] sort1 = sort1(unsorted1);
+        //int[] sort2 = sort1(unsorted1);
+        //int[] sort3 = sort1(unsorted1);
+        
+    }
+    
+    public int[] sort1(int[] unsortedList){
+        return new int[0];
+    }
+    
+    public int[] sort2(int[] unsortedList){
+        return new int[0];
+    }
+    
+    public int[] sort3(int[] unsortedList){
+        return new int[0];
+    }
+}

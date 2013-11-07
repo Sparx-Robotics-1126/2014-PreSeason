2ce33d1db8118df9ff4d0960fd4c36963f5fc685
Justin Bassett <Bassett.JustinT@gmail.com>
2013-11-06 19:12:13 -0500

    Fix project.

--- DemoingProject/src/demoingproject/DemoingProject.java
+++ DemoingProject/src/demoingproject/DemoingProject.java
@@ -19,21 +19,21 @@ public class DemoingProject {
         int[] unsorted1 = new int[10];
         int[] unsorted2 = new int[10];
         int[] unsorted3 = new int[10];
-        //int[] sort1 = sort1(unsorted1);
-        //int[] sort2 = sort1(unsorted1);
-        //int[] sort3 = sort1(unsorted1);
+        int[] sort1 = sort1(unsorted1);
+        int[] sort2 = sort1(unsorted2);
+        int[] sort3 = sort1(unsorted3);
         
     }
     
-    public int[] sort1(int[] unsortedList){
+    public static int[] sort1(int[] unsortedList){
         return new int[0];
     }
     
-    public int[] sort2(int[] unsortedList){
+    public static int[] sort2(int[] unsortedList){
         return new int[0];
     }
     
-    public int[] sort3(int[] unsortedList){
+    public static int[] sort3(int[] unsortedList){
         return new int[0];
     }
 }
